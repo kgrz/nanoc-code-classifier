c3a8c77b286b1b37f4b76b9ba9823341763c5c0c
Josh Bassett <josh.bassett@gmail.com>
2011-01-20 15:46:47 +1100

    Updated filter to parse language and caption tags.

--- lib/nanoc3/filters/code_classifier.rb
+++ lib/nanoc3/filters/code_classifier.rb
@@ -2,28 +2,56 @@ require 'nokogiri'
 
 module Nanoc3
   module Filters
-    # A nanoc filter which pre-processes a code block for a language tag.
+    # A nanoc filter which pre-processes code elements for special tags.
     #
     # For example:
     #
-    #     @language-ruby
+    #     [@language="ruby"]
+    #     [@caption="lorem ipsum"]
     class CodeClassifier < Nanoc3::Filter
       identifier :code_classifier
       type :text
 
       def run(content, params = {})
-        doc = Nokogiri::HTML.fragment(content)
-        doc.css("pre > code").each do |element|
-          element.content = element.content.sub(/\s*@(language-\w+)\s*/) do
-            append_class(element, $1)
-            append_class(element.parent, params[:pre_class)
-            nil
-          end.strip
+        html = Nokogiri::HTML.fragment(content)
+
+        html.xpath("pre/code").each do |code|
+          pre = code.parent
+
+          # Set the class on the <pre>.
+          append_class(pre, params[:pre_class]) if params[:pre_class]
+
+          process_language_tag(code)
+          process_caption_tag(code)
         end
-        doc.to_s
+
+        html.to_s
       end
 
     private
+
+      def process_language_tag(element)
+        element.content = element.content.sub(/\[\s*@language\s*=\s*"([^"]+)"\s*\]/) do
+          append_class(element, "language-#{$1}")
+          nil
+        end.strip!
+      end
+
+      def process_caption_tag(element)
+        element.content = element.content.sub(/\[\s*@caption\s*=\s*"([^"]+)"\s*\]/) do
+          # Wrap in a <figure>.
+          figure = Nokogiri::XML::Node.new("figure", element.document)
+          element.parent.add_next_sibling(figure)
+          element.parent.parent = figure
+
+          # Add a <figcaption>.
+          figcaption = Nokogiri::XML::Node.new("figcaption", element.document)
+          figcaption.content = $1
+          element.parent.add_next_sibling(figcaption)
+
+          nil
+        end.strip!
+      end
 
       def append_class(element, klass)
         element["class"] = ((element["class"] || "") + " " + klass).strip
